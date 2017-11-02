20474221295be9a873516142b6c262b9c2b0cf31
Jewe Pasch <post@jewepasch.de>
2017-11-02 11:10:26 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Blog</title>
+	<link href="https://fonts.googleapis.com/css?family=Dancing+Script" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+
+	<nav>
+		<img src="http://www.livelovemomblog.com/wp-content/uploads/2016/10/LiveLoveMom.png">
+	</nav>
+
+	<div id="header">
+		<h1>Blog</h1>
+		<img src="http://www.livelovemomblog.com/wp-content/uploads/2016/09/MG_9739_WebReady.jpg">
+	</div>
+
+	<div id="content">
+
+		<article>
+			<img src="http://www.livelovemomblog.com/wp-content/uploads/2016/09/IMG_4330_web-1.jpg">
+			<h2><a href="http://apple.com">My First Photo-less Blog Post</a></h2>
+			<p>I'm back! Usually I would go into a long, strung out 'why I've been go so long,' but honestly, there's no need for an explanation. [...]</p>
+			<p class="details">By Cait| June 26th, 2017|About Me, Hanoi, Love & Life, My Family, Our Story, Summer, Travel, Uncategorized, Vietnam|1 Comment
+				<a href="http://www.google.com">Read more ></a>
+			</p>
+		</article>
+		<article>
+			<img src="http://www.livelovemomblog.com/wp-content/uploads/2017/03/DSC_0652_WebReady-1200x800.jpg">
+			<h2><a href="http://apple.com">OUR NEW HOME: HANOI, VIETNAM</a></h2>
+			<p>"Okay, okay... what? I thought you were settled in Nha Trang? What's happening? Why did you decide to move to Hanoi? TELL ME EVERYTHING!" This [...]</p>
+			<p class="details">By Cait| March 20th, 2017|About Me, Hanoi, Love & Life, My Family, Our Story, Travel, Vietnam|4 Comments
+				<a href="http://www.google.com">Read more ></a>
+			</p>
+		</article>
+		<article>
+			<img src="http://www.livelovemomblog.com/wp-content/uploads/2016/09/IMG_4330_web-1.jpg">
+			<h2><a href="http://apple.com">My First Photo-less Blog Post</a></h2>
+			<p>I'm back! Usually I would go into a long, strung out 'why I've been go so long,' but honestly, there's no need for an explanation. [...]</p>
+			<p class="details">By Cait| June 26th, 2017|About Me, Hanoi, Love & Life, My Family, Our Story, Summer, Travel, Uncategorized, Vietnam|1 Comment
+				<a href="http://www.google.com">Read more ></a>
+			</p>
+		</article>
+	</div>
+
+	<div id="scrollUP">SCROLL UP</div>
+
+	<script type="text/javascript">
+		
+		var button = document.querySelector("#scrollUP");
+		button.addEventListener("click",function(){
+			scroll(0,0);
+		})
+
+		document.addEventListener("scroll",function(){
+
+			var button = "jewe";
+
+			if(document.documentElement.scrollTop > 100){
+				button.style.display = "block";
+			} else {
+				button.style.display = "none";
+			}
+		})
+
+	</script>
+
+
+</body>
+</html>
